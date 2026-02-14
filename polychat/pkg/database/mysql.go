57b62b0761d40fb5316b3821ebe4e58e9529894a
iklxy <2661711673@qq.com>
2026-02-14 18:18:02 +0800

    修复小bug

--- polychat/pkg/database/mysql.go
+++ polychat/pkg/database/mysql.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"os"
 	"polychat/internal/model"
 
 	"gorm.io/driver/mysql"
@@ -12,8 +13,13 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := "admin:YY010303@tcp(47.110.94.115:3306)/polychat_db?charset=utf8mb4&parseTime=True&loc=Local&timeout=10s&readTimeout=30s&writeTimeout=30s&allowNativePasswords=true&tls=false"
+	// 获取数据库Host，默认为远程IP（用于本地开发），部署到服务器时可通过环境变量 DB_HOST=127.0.0.1 指定
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "47.110.94.115"
+	}
+
+	dsn := fmt.Sprintf("admin:YY010303@tcp(%s:3306)/polychat_db?charset=utf8mb4&parseTime=True&loc=Local&timeout=10s&readTimeout=30s&writeTimeout=30s&allowNativePasswords=true&tls=false", dbHost)
 
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
